1681069b50f1ae07fc6fa04458ef483c689b5204
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2022-09-28 11:47:29 +0000

    proxy works with express

--- index.mjs
+++ index.mjs
@@ -1,19 +1,20 @@
 import createServer from '@tomphttp/bare-server-node';
 import http from 'http';
 import express from 'express';
+import cookie_parser from "cookie-parser";
 const app = express();
 
 const bare =  createServer('/bare/');
 app.use(express.static(process.cwd() + "/static"))
 
-const server = http.createServer(app);
+const server = http.createServer();
 
 server.on('request', (req, res) => {
-    if (bare.shouldRoute(req)) {
+  if (bare.shouldRoute(req)) {
 		bare.routeRequest(req, res);
-	}// else {
-	//serve.serve(req, res);
-//	}
+	} else {
+	  app(req, res);
+}
 });
 
 server.on('upgrade', (req, socket, head) => {
