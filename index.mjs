32379e51e360916ff2f8a54cc0a6dd1dd55d3e38
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2022-09-28 10:58:42 +0000

    serve static with express but tomp bare (proxy) is broken

--- index.mjs
+++ index.mjs
@@ -1,25 +1,25 @@
 import createServer from '@tomphttp/bare-server-node';
 import http from 'http';
-import nodeStatic from 'node-static';
-
+import express from 'express';
+const app = express();
 
 const bare =  createServer('/bare/');
-const serve = new nodeStatic.Server('static/');
+app.use(express.static(process.cwd() + "/static"))
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.on('request', (req, res) => {
     if (bare.shouldRoute(req)) {
 		bare.routeRequest(req, res);
-	} else {
-		serve.serve(req, res);
-	}
+	}// else {
+	//serve.serve(req, res);
+//	}
 });
 
 server.on('upgrade', (req, socket, head) => {
 	if (bare.shouldRoute(req, socket, head)) {
 		bare.routeUpgrade(req, socket, head);
-	}else{
+	} else {
 		socket.end();
 	}
 });
