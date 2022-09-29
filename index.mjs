9a1ddc52d3ce4e92da120f3b61b28538a49d932f
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2022-09-29 17:13:41 +0000

    True auth cookie check for websockets

--- index.mjs
+++ index.mjs
@@ -2,6 +2,8 @@ import createServer from '@tomphttp/bare-server-node';
 import http from 'http';
 import express from 'express';
 import cookie_parser from "cookie-parser";
+import * as signature from "cookie-signature";
+import * as cookie from "cookie";
 const app = express();
 
 const bare =  createServer('/bare/');
@@ -22,17 +24,17 @@ app.get("/", (req, res) => {
       let step1 = new Buffer.from(auth_Stuff.split(" ")[1], 'base64')
       let step2 = step1.toString().split(":")
       if (step2[0] == process.env.NAME_ADMIN && step2[1] == process.env.PASS_ADMIN) {
-        console.log("New login: admin")
+        console.log("[EXPRESS] New login: admin")
         res.cookie('user', 'admin', { signed: true })
         res.sendFile(process.cwd() + "/static/index.html")
       }
       else if (step2[0] == process.env.NAME_TIER1 && step2[1] == process.env.PASS_TIER1) {
-        console.log("New login: tier 1")
+        console.log("[EXPRESS] New login: tier 1")
         res.cookie('user', 'tier 1', { signed: true })
         res.sendFile(process.cwd() + "/static/index.html")
       }
       else if (step2[0] == process.env.NAME_TIER2 && step2[1] == process.env.PASS_TIER2) {
-        console.log("New login: tier 2")
+        console.log("[EXPRESS] New login: tier 2")
         res.cookie('user', 'tier 2', { signed: true })
         res.sendFile(process.cwd() + "/static/index.html")
       }
@@ -44,15 +46,15 @@ app.get("/", (req, res) => {
   }
   else {
     if (req.signedCookies.user=='admin') {
-      console.log("Returning login: admin")
+      console.log("[EXPRESS] Returning login: admin")
       res.sendFile(process.cwd() + "/static/index.html")
     }
     else if (req.signedCookies.user=='tier 1') {
-      console.log("Returning login: tier 1")
+      console.log("[EXPRESS] Returning login: tier 1")
       res.sendFile(process.cwd() + "/static/index.html")
     }
     else if (req.signedCookies.user=='tier 2') {
-      console.log("Returning login: tier 2")
+      console.log("[EXPRESS] Returning login: tier 2")
       res.sendFile(process.cwd() + "/static/index.html")
     }
     else {
@@ -75,8 +77,19 @@ server.on('request', (req, res) => {
 });
 
 server.on('upgrade', (req, socket, head) => {
-  console.log(req.headers)
-  if (!req.headers.cookie.startsWith("user=")) return socket.end();
+  //console.log(req.headers)
+  if (!req.headers.cookie) {
+    console.log("[WEBSOCKET PROXY] Upgrade request without any cookies.")
+    return socket.end();
+  }
+  let cookies = cookie.parse(req.headers.cookie);
+  //console.log(cookies)
+  let unsigned = signature.unsign(cookies[Object.keys(cookies)[0]].slice(2), process.env.COOKIESALT);
+  //console.log(unsigned);
+  if (!(unsigned == "admin" || unsigned == "tier 1" || unsigned == "tier 2")) {
+    console.log("[WEBSOCKET PROXY] Unauthorised upgrade, potentially using false cookie!");
+    return socket.end();
+  }
 	if (bare.shouldRoute(req, socket, head)) {
 		bare.routeUpgrade(req, socket, head);
 	} else {
